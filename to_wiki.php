13b3a720caf320f9f898fa1a77f7c11ae1da713e
Eccenux <eccenux+git@gmail.com>
2020-01-30 00:28:48 +0100

    json to wiki

---
+++ to_wiki.php
@@ -0,0 +1,65 @@
+<?php
+/**
+	JSON to wiki-code.
+	
+	array (
+	  'actor_name' => 0,
+	  'review_count' => 1,
+	  'review_count_intial' => 2,
+	  'review_count_total' => 3,
+	)	
+*/
+
+date_default_timezone_set('Europe/Warsaw');
+
+$inputPath = 'quarry-reviewers-pl-2019.json';
+$outputPath = "reviewers-pl.wiki";
+
+//trigger_error("debugger", E_USER_NOTICE);
+
+$data = json_decode(file_get_contents($inputPath));
+$columns = array_flip($data->headers);
+
+// mapping helper
+class TopReviewer {
+	public $actor_name;
+	public $review_count;
+	public $review_count_intial;
+	public $review_count_total;
+	
+	function __construct($row) {
+		global $columns;
+		
+		$this->actor_name = $row[$columns['actor_name']];
+		$this->review_count = $row[$columns['review_count']];
+		$this->review_count_intial = $row[$columns['review_count_intial']];
+		$this->review_count_total = $row[$columns['review_count_total']];
+	}
+}
+
+/**
+$r = new TopReviewer($data->rows[0]);
+var_export($r);
+var_export($columns);
+die();
+/**/
+
+$numRow = 0;
+$wiki = '';
+echo "\n[INFO] Strating loop...";
+foreach($data->rows as $row) {
+	$numRow++;
+	
+	// top600
+	if ($numRow > 600) {
+		break;
+	}
+	$r = new TopReviewer($row);
+	
+	$wiki .= "\n|-\n| $numRow || [[User:{$r->actor_name}|{$r->actor_name}]] || {$r->review_count} || {$r->review_count_intial} || {$r->review_count_total}";
+}
+$wiki .=  "\n|}\n";
+
+file_put_contents($outputPath, $wiki);
+
+echo "\n[INFO] Done";
