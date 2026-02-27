f95c819f248d395ae7d7a8d6209194560fc4fa2c
eccenux <eccenux+git@gmail.com>
2026-02-27 17:57:04 +0100

    parse all files in one go

--- to_wiki.php
+++ to_wiki.php
@@ -12,22 +12,21 @@
 
 date_default_timezone_set('Europe/Warsaw');
 
+$inputPaths = [];
+$outputPaths = [];
 /**
-$inputPath = 'quarry-reviewers-pl-2019.json';
-$outputPath = "reviewers-pl.wiki";
+$inputPaths[] = 'quarry-reviewers-pl-2019.json';
+$outputPaths[] = "reviewers-pl.wiki";
 /**/
 $year = "2025";
-$inputPath = "quarry-reviewers-pl-$year.json";
-$outputPath = "reviewers-pl-$year.wiki";
+$inputPaths[] = "quarry-reviewers-pl-$year.json";
+$outputPaths[] = "reviewers-pl-$year.wiki";
 /**/
-$inputPath = 'quarry-reviewers-pl-all.json';
-$outputPath = "reviewers-pl-all.wiki";
+$inputPaths[] = 'quarry-reviewers-pl-all.json';
+$outputPaths[] = "reviewers-pl-all.wiki";
 /**/
 
 //trigger_error("debugger", E_USER_NOTICE);
-
-$data = json_decode(file_get_contents($inputPath));
-$columns = array_flip($data->headers);
 
 // mapping helper
 class TopReviewer {
@@ -36,15 +35,34 @@ class TopReviewer {
 	public $review_count_intial;
 	public $review_count_total;
 	
-	function __construct($row) {
-		global $columns;
-		
+	function __construct($row, $columns) {
 		$this->actor_name = $row[$columns['actor_name']];
 		$this->review_count = number_format((int)$row[$columns['review_count']], 0, '', ' ');
 		$this->review_count_intial = number_format((int)$row[$columns['review_count_intial']], 0, '', ' ');
 		$this->review_count_total = number_format((int)$row[$columns['review_count_total']], 0, '', ' ');
 	}
 }
+
+// Convert all PHP errors (incl. warnings) to ErrorException
+set_error_handler(function (
+	int $severity,
+	string $message,
+	string $file,
+	int $line
+) {
+	// Respect @-operator
+	if (!(error_reporting() & $severity)) {
+		return false;
+	}
+
+	throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+// Ensure warnings are reported
+error_reporting(E_ALL);
+
+// Optional: don’t hide them
+ini_set('display_errors', '1');
 
 /**
 $r = new TopReviewer($data->rows[0]);
@@ -53,24 +71,38 @@ var_export($columns);
 die();
 /**/
 
-$numRow = 0;
-$wiki = '';
-echo "\n[INFO] Strating loop...";
-foreach($data->rows as $row) {
-	$numRow++;
-	$r = new TopReviewer($row);
-	
-	// top600, but make sure we show all users tied on last place
-	if ($numRow > 600 && $prev != $r->review_count) {
-		break;
+function parseJsonToWiki ($inputPath, $outputPath) {
+	echo "\n[" . date('c') . "] [INFO] Loading $inputPath...";
+	$data = json_decode(file_get_contents($inputPath));
+	$columns = array_flip($data->headers);
+
+	$numRow = 0;
+	$wiki = '';
+	echo "\n[" . date('c') . "] [INFO] Strating loop for $inputPath...";
+	foreach($data->rows as $row) {
+		$numRow++;
+		$r = new TopReviewer($row, $columns);
+		
+		// top600, but make sure we show all users tied on last place
+		if ($numRow > 600 && $prev != $r->review_count) {
+			break;
+		}
+		
+		$wiki .= "\n|-\n| $numRow || [[User:{$r->actor_name}|{$r->actor_name}]] || {$r->review_count} || {$r->review_count_intial} || {$r->review_count_total}";
+
+		$prev = $r->review_count;
 	}
-	
-	$wiki .= "\n|-\n| $numRow || [[User:{$r->actor_name}|{$r->actor_name}]] || {$r->review_count} || {$r->review_count_intial} || {$r->review_count_total}";
+	$wiki .=  "\n|}\n";
 
-	$prev = $r->review_count;
+	file_put_contents($outputPath, $wiki);
+
+	echo "\n[" . date('c') . "] [INFO] Done";
 }
-$wiki .=  "\n|}\n";
 
-file_put_contents($outputPath, $wiki);
+//
+// Main loop
+foreach ($inputPaths as $i => $inputPath) {
+	$outputPath = $outputPaths[$i] ?? null;
 
-echo "\n[INFO] Done";
+	parseJsonToWiki ($inputPath, $outputPath);
+}
